4e3d0e65b45d914ceb128132ff3ad7469bcbd8fb
Ola M <example@example.com>
2023-07-27 14:33:16 +0200

    add failing test
    Remove dependency on oauth2 to make the test pass

--- src/test/kotlin/com/example/JsonTest.kt
+++ src/test/kotlin/com/example/JsonTest.kt
@@ -1,12 +1,18 @@
 package com.example
-import io.micronaut.runtime.EmbeddedApplication
-import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
+
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
 
 @MicronautTest
-class JsonTest(private val application: EmbeddedApplication<*>): StringSpec({
+class JsonTest(@Client("/") val client: HttpClient) : StringSpec({
 
-    "test the server is running" {
-        assert(application.isRunning)
+
+    "response should be sent" {
+        val resp: String = client.toBlocking().retrieve("/my-controller")
+
+        resp shouldBe """{"aproperty":"Hello World"}"""
     }
 })
